f54004aa3bcad46bb5fe461bc47a31fd09665290
Kermit2626 <oligray26@gmail.com>
2023-03-01 13:08:15 +0000

    input tooltip popup

--- src/components/ProjectInputBox.tsx
+++ src/components/ProjectInputBox.tsx
@@ -1,16 +1,22 @@
 import { FC } from 'react';
 import { Dispatch, SetStateAction } from "react";
 
+import { Tooltip } from '@mui/material';
+import { Zoom } from '@mui/material';
+
 interface inputInterface {
     inputText: string;
     changeState: Dispatch<SetStateAction<string>>;
+    description: string;
 }
 
 const ProjectInputBox:FC<inputInterface> = (props) =>    {
     
     return (
         <div className='w-full flex flex-row my-1'>
-            <div className='w-1/3 p-2 hover:cursor-default'>{props.inputText}</div>
+            <Tooltip title={props.description} placement={'left'} TransitionComponent={Zoom}>
+                <div className='w-1/3 p-2 hover:cursor-default'>{props.inputText}</div>
+            </Tooltip>
             <input 
                 className='w-2/3 bg-black rounded-xl p-2 focus:outline-none'
                 type="text"  
--- src/components/modules/CreateProject.tsx
+++ src/components/modules/CreateProject.tsx
@@ -1,5 +1,4 @@
 import { FC } from "react";
-
 import { useState, useEffect } from "react";
 
 import ProjectInputBox from "../ProjectInputBox"; 
@@ -11,15 +10,57 @@ const CreateProjectModule:FC = () => {
     const [mintPrice, setMintPrice] = useState('');
     const [maxPerWallet, setMaxPerWallet] = useState('');
 
+    const inputButtons = [
+        {
+            id: 1,
+            text: 'PROJECT NAME',
+            changeState: setProjectName,
+            description: `Set the name of your NFT project`
+        },
+        {
+            id: 2,
+            text: 'PROJECT ABBREVIATION',
+            changeState: setProjectName,
+            description: `Set the abbreviation of your NFT project`
+        },
+        {
+            id: 3,
+            text: 'MAX SUPPLY',
+            changeState: setProjectName,
+            description: `Set how many pieces are art can be purchased 
+            from your NFT project`
+        },
+        {
+            id: 4,
+            text: 'MINT PRICE',
+            changeState: setProjectName,
+            description: `Set the price to purchase an NFT from your project`
+        },
+        {
+            id: 5,
+            text: 'MAX PER WALLET',
+            changeState: setProjectName,
+            description: `Set how many NFTs can be purchased with the use of a 
+            single wallet`
+        }
+    ]
+
     return(
         <div className="m-auto justify-center w-1/2 flex flex-col bg-top-nav rounded-xl p-5">
             <div className="text-2xl ml-10 mb-5">Project Configuration</div>
 
-            <ProjectInputBox inputText={'PROJECT NAME'} changeState={setProjectName} />
-            <ProjectInputBox inputText={'PROJECT ABBREVIATION'} changeState={setProjectAbbreviation} />
-            <ProjectInputBox inputText={'MAX SUPPLY'} changeState={setMaxSupply} />
-            <ProjectInputBox inputText={'MINT PRICE'} changeState={setMintPrice} />
-            <ProjectInputBox inputText={'MAX PER WALLET'} changeState={setMaxSupply} />
+            {
+                inputButtons.map(element => {
+                    return (
+                        <ProjectInputBox 
+                            key={element.id} 
+                            inputText={element.text}
+                            changeState={element.changeState}
+                            description={element.description} 
+                        />
+                    )
+                })
+            }
     
             <p className="p-3">
                 Enter in your configuration for the NFT collection you would
