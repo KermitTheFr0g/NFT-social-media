7eed64ad4ecce8fca5f68a7e81192cd6d1608afc
Kermit2626 <oligray26@gmail.com>
2023-03-09 12:22:50 +0000

    new compiler and deployer

--- utils/contract/compile.js
+++ utils/contract/compile.js
@@ -1,35 +1,76 @@
-// 1. Import packages
 const fs = require('fs');
-const path = require('path');
 const solc = require('solc');
 
-// 2. Get path and load contract
-const contractPath = path.resolve(__dirname, "Incrementer.sol");
-const source = fs.readFileSync(contractPath, "utf8");
+// contractPath: "../../contract_0x51--etc.sol"
+const compileContract = (contractPath, className) => {
+    const sources = {}
+    compileImports(contractPath, sources);
 
-// 3. Create input object
-const input = {
-   language: 'Solidity',
-   sources: {
-      'Incrementer.sol': {
-         content: source,
+    var input = {
+      language: "Solidity",
+      sources: sources,
+      settings: {
+        outputSelection: {
+          "*": {
+            "*": ["*"],
+          },
+        },
       },
-   },
-   settings: {
-      outputSelection: {
-         '*': {
-            '*': ['*'],
-         },
-      },
-   },
-};
+    };
+  
+  
+    const tempFile = JSON.parse(solc.compile(JSON.stringify(input)));
+    const contractFile = tempFile.contracts[contractPath][className];
+    // exports the abi 
+    fs.writeFileSync(`../../user_contracts_abi/${getWalletAddress(contractPath)}_abi.json`, JSON.stringify(contractFile.abi));
+  
+    return contractFile;
+}
 
-// 4. Compile the contract
-const tempFile = JSON.parse(solc.compile(JSON.stringify(input)));
-console.log(tempFile);
-const contractFile = tempFile.contracts['Incrementer.sol']['Incrementer'];
-// exports the abi 
-fs.writeFileSync('contract_abi.json', JSON.stringify(contractFile.abi));
+const compileImports = (root, sources) => {
+    sources[root] = { content: fs.readFileSync(root, 'utf-8')};
+    const imports = getLibraryImports(root);
+    for (let i = 0; i < imports.length; i++) {
+        compileImports(imports[i], sources);
+    }
+}
 
-// 5. Export contract data
-module.exports = contractFile;
+const getLibraryImports = (path) => {
+    const file = fs.readFileSync(path, 'utf-8');
+    const files = new Array();
+    file
+        .toString()
+        .split('\n')
+        .forEach((line, index, arr) => {
+            if((index == arr.length - 1 && line == "") ||
+            !line.trim().startsWith('import')
+            ) {
+                return;
+            }
+
+            const relativePath = line.substring(8, line.length - 2);
+            const fullPath = completeFullPath(path, relativePath);
+            files.push(fullPath);
+        })
+    return files;
+}
+
+const completeFullPath = (parent, path) => {
+    let curDir = parent.substr(0, parent.lastIndexOf("/")); //i.e. ./node/.../ERC721
+    if (path.startsWith("./")) {
+      return curDir + "/" + path.substr(2);
+    }
+  
+    while (path.startsWith("../")) {
+      curDir = curDir.substr(0, curDir.lastIndexOf("/"));
+      path = path.substr(3);
+    }
+  
+    return curDir + "/" + path;
+}
+
+const getWalletAddress = (contractPath) => {
+    return contractPath.split('/')[3].split('.')[0]
+}
+
+module.exports = compileContract;
--- utils/contract/deploy.js
+++ utils/contract/deploy.js
@@ -1,63 +1,40 @@
 require('dotenv').config({ path: '../../.env' });
-
-// 1. Import the contract file
-const contractFile = require('./test');
-
-// 2. Add the Ethers provider logic here:
-// 1. Import ethers
+const contractCompiler = require('./compile');
 const ethers = require('ethers');
 
-// 2. Define network configurations
+//todo look into transfering ownership of contract to new user
+
+// url for sepolia rpc
 const url = 'https://rpc2.sepolia.org';
-const provider = new ethers.providers.JsonRpcProvider(url)
+// provider for sepolia
+const provider = new ethers.providers.JsonRpcProvider(url);
 
-// 3. Create account variables
+// account with funds to deploy contract
 const account_from = {
-  privateKey: process.env.PRIVATE,
-};
-
-// 4. Save the bytecode and ABI
-const bytecode = contractFile.evm.bytecode.object;
-const abi = contractFile.abi;
-
-// 5. Create wallet
-let wallet = new ethers.Wallet(account_from.privateKey, provider);
-
-// 6. Create contract instance with signer
-const incrementer = new ethers.ContractFactory(abi, bytecode, wallet);
-
-// 7. Create deploy function
-const deploy = async () => {
-  console.log(`Attempting to deploy from account: ${wallet.address}`);
-
-  // 8. Send tx (initial value set to 5) and wait for receipt
-  //todo enter in params for the 
-  const contract = await incrementer.deploy('ipfs://123', 'ipfs://134');
-  const txReceipt = await contract.deployTransaction.wait();
-
-
-  console.log(`Contract deployed at address: ${txReceipt.contractAddress}`);
-};
-
-const transfer = async (contractAddress) => {
-  const newAddress = '0x516923E55e9eD4Bcf08CFA4A477a11805b0CD72C'
-  console.log(`Attempting to transfer contract to: ${newAddress}`);
-
-  const deployedContract = new ethers.Contract('0xb8eC1875b89042939464f8a169f8A9028887D386', abi, wallet);
-
-  console.log(await deployedContract.transferOwnership(newAddress))
+    privateKey: process.env.PRIVATE,
 }
 
-const mint = async () => {
-  console.log(`Attempting to mint from: ${'0xb8eC1875b89042939464f8a169f8A9028887D386'}`);
+const deployContract = async (contractPath, className, userAddress, ipfsAddress) => {
+    const contractFile = contractCompiler(contractPath, className);
 
-  const deployedContract = new ethers.Contract('0xb8eC1875b89042939464f8a169f8A9028887D386', abi, wallet);
+    // bytecode and abi from compiled contract
+    const bytecode = contractFile.evm.bytecode.object;
+    const abi = contractFile.abi;
 
-  console.log(await deployedContract.toggleMint());
-  console.log(await deployedContract.publicMint('0x516923E55e9eD4Bcf08CFA4A477a11805b0CD72C', '1'));
+    // access wallet to deploy contract
+    let wallet = new ethers.Wallet(account_from.privateKey, provider);
+
+    // logic for deploying contract
+    const userContractFactory = new ethers.ContractFactory(abi, bytecode, wallet);
+
+    const contract = await userContractFactory.deploy(ipfsAddress, ipfsAddress);
+    const txReceipt = await contract.deployTransaction.wait();
+
+    console.log(`Contract deployed at address: ${txReceipt.contractAddress}`);
+
+    return {
+        contractAddress: txReceipt.contractAddress
+    }
 }
 
-// 9. Call the deploy function
-//deploy();
-//transfer();
-mint()
+module.exports = deployContract;
