ece8de4afcc73958de1eb846c96aeca3517c65d9
Kermit2626 <oligray26@gmail.com>
2023-03-19 22:52:36 +0000

    Create index.tsx

---
+++ src/pages/discover/index.tsx
@@ -0,0 +1,17 @@
+import { FC } from "react";
+import Head from 'next/head';
+
+import TopNav from "@/components/modules/TopNav";
+
+const Discover:FC = () => {
+    return (
+        <>
+            <Head>
+                <title>Discover</title>
+            </Head>
+
+            
+
+        </>
+    )
+}
