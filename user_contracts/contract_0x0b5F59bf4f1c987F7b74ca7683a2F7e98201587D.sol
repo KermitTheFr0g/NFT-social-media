39bb76d9aedf0863253310e7aa7755c7ad52c99f
Kermit2626 <oligray26@gmail.com>
2023-03-21 21:35:32 +0000

    Update contract_0x0b5F59bf4f1c987F7b74ca7683a2F7e98201587D.sol

--- user_contracts/contract_0x0b5F59bf4f1c987F7b74ca7683a2F7e98201587D.sol
+++ user_contracts/contract_0x0b5F59bf4f1c987F7b74ca7683a2F7e98201587D.sol
@@ -3,16 +3,16 @@ pragma solidity ^0.8.4;
 import "../node_modules/@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "../node_modules/@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
-contract TestingProject is ERC721, ERC721URIStorage, Ownable {
+contract BlackCock is ERC721, ERC721URIStorage, Ownable {
     uint256 public totalSupply; 
     bool public mintEnabled;
-    uint256 public maxSupply = 10;
+    uint256 public maxSupply = 1000;
     uint256[] public tokenIDs;
     mapping(address => uint256) public mintedWallets;
     string public baseURI;
     constructor(
       string memory _initBaseURI
-    ) ERC721("Testing Project", "TP") {
+    ) ERC721("BlackCock", "BC") {
         baseURI = _initBaseURI;
     }
     
@@ -39,7 +39,7 @@ contract TestingProject is ERC721, ERC721URIStorage, Ownable {
         uint256 amount
     ) public payable {
         require(mintEnabled, "Minting is not yet enabled");
-        require(mintedWallets[msg.sender] + amount <= 1, "You cannot mint more than 1!");
+        require(mintedWallets[msg.sender] + amount <= 10, "You cannot mint more than 10!");
         require(totalSupply + amount <= maxSupply, "Max NFTS have been reached");
         require(msg.value == 0.01 ether, "This NFT costs 0.01 ETH");
         
