711f375d472ab4ebad47b3e90f4451769fbad25d
Kermit2626 <oligray26@gmail.com>
2023-03-23 08:52:03 +0000

    Update DiscoverProjects.tsx

--- src/components/modules/DiscoverProjects.tsx
+++ src/components/modules/DiscoverProjects.tsx
@@ -1,9 +1,27 @@
 import { FC, useEffect, useState } from "react";
 
 const DiscoverProjects:FC = () => {
+    const [loading, setLoading] = useState(false);
+    const [projects, setProjects] = useState([]);
     
     useEffect(() => {
+        setLoading(true)
+
+        const getData = async () => {
+            const response = fetch(`/api/discover`, {
+                method: 'GET',
+                headers: { 'Content-Type': 'application/json' }
+            });
+
+            const discoveryData = await (await response).json();
         
+        
+            console.log(discoveryData);
+        }
+
+        getData();
+
+        setLoading(false);
     }, [])
 
     return (
