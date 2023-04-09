cab8a7a06e84b2e94037b01a60f916ad870865a7
Kermit2626 <oligray26@gmail.com>
2023-04-09 22:02:18 +0100

    Update CreateProject.tsx

--- src/components/modules/CreateProject.tsx
+++ src/components/modules/CreateProject.tsx
@@ -2,7 +2,6 @@ import { FC } from "react";
 import { useState, useEffect } from "react";
 
 import { Tooltip } from '@mui/material';
-import { Snackbar } from '@mui/material';
 
 import ProjectInputBox from "../ProjectInputBox"; 
 
