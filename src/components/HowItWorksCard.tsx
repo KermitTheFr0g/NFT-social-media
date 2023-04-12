9804fa2f67301d1de1825cd51bdd5028dd009edb
Kermit2626 <oligray26@gmail.com>
2023-04-12 21:56:03 +0100

    main page

--- src/components/HowItWorksCard.tsx
+++ src/components/HowItWorksCard.tsx
@@ -9,7 +9,7 @@ interface cardInterface {
 const HowItWorksCard: FC<cardInterface> = (props) => {
 
     return (
-        <div className='p-10 w-1/4'>
+        <div className='p-5 m-5 w-1/4 bg-top-nav rounded-xl'>
             <div className='text-2xl'>Step {props.step}</div>
             <div className='text-3xl'>{props.title}</div>
             <p className='text-xl'>
