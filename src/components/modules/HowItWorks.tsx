97d484a580d5738200a320c0abc54331d593ba02
Kermit2626 <oligray26@gmail.com>
2023-04-12 21:52:30 +0100

    how it works

--- src/components/modules/HowItWorks.tsx
+++ src/components/modules/HowItWorks.tsx
@@ -1,5 +1,6 @@
 import { FC } from "react";
 
+import HowItWorksCard from "../HowItWorksCard";
 
 const HowItWorks: FC = () => {
     const steps = [
@@ -24,14 +25,31 @@ const HowItWorks: FC = () => {
     ]
 
     return (
-        <div className="w-1/2 m-auto">
-            <div className="text-5xl">How it Works</div>
-            <p className="">
-                Lorem Ipsum is simply dummy text of the printing and typesetting 
-                industry. Lorem Ipsum has been the industry's standard dummy 
-                text ever since the 1500s, when an unknown printer took a galley 
-                of type and scrambled it to make a type specimen book.
-            </p>
+        <div>
+            <div className="w-1/2 p-32">
+                <div className="text-5xl">How it Works</div>
+                <p className="w-11/12 mt-4">
+                    Lorem Ipsum is simply dummy text of the printing and typesetting
+                    industry. Lorem Ipsum has been the industry's standard dummy
+                    text ever since the 1500s, when an unknown printer took a galley
+                    of type and scrambled it to make a type specimen book.
+                </p>
+            </div>
+
+            <div className="flex flex-row justify-center">
+                {
+                    steps.map((step) => {
+                        return (
+                            <HowItWorksCard
+                                key={step.id}
+                                step={step.step}
+                                title={step.title}
+                                description={step.description}
+                            />
+                        )
+                    })
+                }
+            </div>
         </div>
     )
 }
