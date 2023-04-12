0720c84a490e922661bdf49a80a7a97223f481ed
Kermit2626 <oligray26@gmail.com>
2023-04-12 21:52:10 +0100

    how it works card

---
+++ src/components/HowItWorksCard.tsx
@@ -0,0 +1,22 @@
+import { FC } from 'react';
+
+interface cardInterface {
+    step: number;
+    title: string;
+    description: string;
+}
+
+const HowItWorksCard: FC<cardInterface> = (props) => {
+
+    return (
+        <div className='p-10 w-1/4'>
+            <div className='text-2xl'>Step {props.step}</div>
+            <div className='text-3xl'>{props.title}</div>
+            <p className='text-xl'>
+                {props.description}
+            </p>
+        </div>
+    )
+}
+
+export default HowItWorksCard;
